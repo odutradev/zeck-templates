6f53ed8767d290ac162ac708a341b2d5d49636a3
dutra-pc <joao.vitornl@gmail.com>
2024-10-15 10:56:45 -0300

    :sparkles: feat: Create router component

---
+++ templates/react-js/src/routes/index.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+import Error from "../pages/error";
+import Main from '../pages/main';
+
+const Router = () => {
+    return(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navigate to="/404" replace/>}/>
+          <Route path="/404" element={<Error />}/>
+          <Route path="/" element={<Main />}/>
+        </Routes>
+      </BrowserRouter>
+    );
+};
+
+export default Router;
