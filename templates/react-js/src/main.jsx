77f6b0f55c8e886a36bc78ccd747037d33f6ec08
dutra-pc <joao.vitornl@gmail.com>
2024-10-15 22:56:14 -0300

    :sparkles: feat: Add router component in main

--- templates/react-js/src/main.jsx
+++ templates/react-js/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
+import Router from './routes/index.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
-)
+);
