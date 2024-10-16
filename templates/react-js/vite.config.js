736d42c6e7006a8061d7e7db43a88e3e0ed04205
dutra-pc <joao.vitornl@gmail.com>
2024-10-16 11:06:22 -0300

    :wrench: chore: Create and configure  path aliases

--- templates/react-js/vite.config.js
+++ templates/react-js/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@actions': path.resolve(__dirname, './src/actions'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
+});
